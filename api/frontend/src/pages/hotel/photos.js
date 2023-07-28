c00a54e5b084952d6297c3d84749f667d3f6d9ae
1livesinc <1livesinc@gmail.com>
2023-07-28 13:54:42 +0400

    Initial commit of source code

---
+++ api/frontend/src/pages/hotel/photos.js
@@ -0,0 +1,26 @@
+const photos =  [
+
+
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+    {
+        src: "https://media.kvilr.com/wp-content/uploads/2023/07/amstadam.jpeg"
+    },
+
+   
+]
+
+export default photos;
